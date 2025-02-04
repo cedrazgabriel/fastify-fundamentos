6904d863c2391407631ab25a7a73a0741373bc87
Gabriel Cedraz <cedrazdev@gmail.com>
2025-02-04 11:06:54 -0300

    pegando os dados da requisição

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,19 @@ import Fastify from "fastify";
 const fastify = Fastify()
 
 //Forma mais utilizada para declarar rotas no fastify
-fastify.get("/", async () => {
-    return { message: "Hello World" }
+fastify.get("/", async (request) => {
+    const {
+        body,
+        headers,
+        query,
+        params
+    } = request // conseguimos pegar os dados da requisição
+    return {
+        params,
+        query,
+        body,
+        headers
+    }
 })
 
 //Outra forma de declarar rotas no fastify
